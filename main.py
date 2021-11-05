a3ee41cdd5c5327edb804f2eff53f22af8d67b68
adsdsdsas <bizon1133@gmail.com>
2021-11-05 10:04:01 +0100

    updated main.py, check it out!

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+import this
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+print(f'\n{this.s}\n')
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+love = this
+
+print(this is love)
+print(love is True)
+print(love is False)
+print(love is not True or False, love is love)
